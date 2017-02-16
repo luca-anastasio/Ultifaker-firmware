7367fe0ef06750570dc19181a9edba2aabafdfe4
Luca Anastasio <anastasio.lu@gmail.com>
2017-02-16 23:50:31 +0100

    pins

--- pins_ULTIMAKER_LUCA.h
+++ pins_ULTIMAKER_LUCA.h
@@ -12,21 +12,24 @@
 
 #define X_STEP_PIN 43
 #define X_DIR_PIN 45
+#define X_ENABLE_PIN 41
+
 #define X_MIN_PIN 22
 #define X_MAX_PIN 24
-#define X_ENABLE_PIN 41
 
 #define Y_STEP_PIN 25
 #define Y_DIR_PIN 23
+#define Y_ENABLE_PIN 27
+
 #define Y_MIN_PIN 26
 #define Y_MAX_PIN 28
-#define Y_ENABLE_PIN 27
 
 #define Z_STEP_PIN 37
 #define Z_DIR_PIN 39
+#define Z_ENABLE_PIN 35
+
 #define Z_MIN_PIN 30
 #define Z_MAX_PIN 32
-#define Z_ENABLE_PIN 35
 
 #define HEATER_BED_PIN 4
 #define TEMP_BED_PIN 10
