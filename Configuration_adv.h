75fc2fd57e5a606a6ef0d22150c63851bdc6fec5
Luca Anastasio <anastasio.lu@gmail.com>
2017-03-06 17:37:16 +0100

    pin changed

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -69,8 +69,8 @@
 
 //  extruder run-out prevention.
 //if the machine is idle, and the temperature over MINTEMP, every couple of SECONDS some filament is extruded
-//#define EXTRUDER_RUNOUT_PREVENT
-#define EXTRUDER_RUNOUT_MINTEMP 190
+#define EXTRUDER_RUNOUT_PREVENT
+#define EXTRUDER_RUNOUT_MINTEMP 180
 #define EXTRUDER_RUNOUT_SECONDS 30.
 #define EXTRUDER_RUNOUT_ESTEPS 14. //mm filament
 #define EXTRUDER_RUNOUT_SPEED 1500.  //extrusion speed
